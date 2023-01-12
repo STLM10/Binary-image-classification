3851d670eb156e42b6243f53945c7af67e738658
Vikram Anand <101736189+STLM10@users.noreply.github.com>
2023-01-13 00:34:27 +0530

    Update Page.py

--- Page.py
+++ Page.py
@@ -31,6 +31,9 @@ if st.button('CHECK'):
         if probability<0.60:
             st.write('We are little confused here as we are only ', probability*100, '% sure that its a Dog')
             st.header('DOG')
+            st.markdown("Did we guessed it right ?")
+            if st.button('YES'):
+                st.write("YAY !!!")
             
         else:
             st.write('We are ', probability*100, '% sure that its a Dog')
@@ -42,10 +45,6 @@ if st.button('CHECK'):
     image1 = image1/255.0
 
     st.image(image1, width=500)
-    st.markdown("Did we guessed it right ?")
-    if st.button('YES'):
-        st.write("YAY !!!")
-    if st.button('NO'):
-        st.write("We are very sorry, we will try make better prediction in future")
+  
     
 
